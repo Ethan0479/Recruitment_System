1aa5eee21559f5970de799a58624fb7d0b1fa9ad
XINRUDEKEKE <350577273@qq.com>
2019-08-09 22:07:39 +0800

    修改新生的登录，注册，个人信息修改的views,form和html

--- apps/freshman/models.py
+++ apps/freshman/models.py
@@ -1,8 +1,6 @@
 # _*_ encoding:utf-8 _*_
-
-
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 from ckeditor_uploader.fields import RichTextUploadingField
 
 
@@ -17,7 +15,9 @@ class GlobalSettings(object):
 class Freshman(models.Model):   # 此处继承AbstractUser， 改写auth_user表，以便登录
 
     # 学号， 姓名， 性别， 学院， 专业， 专业班级， 手机号， QQ， 邮箱， 方向为必填， 申请时填入
+
     newstudent_id = models.CharField(max_length=11, verbose_name=u'学号')#username存储学号
+    password = models.CharField(max_length=25, verbose_name='密码')
     newname = models.CharField(max_length=50, verbose_name=u'姓名')
     gender = models.CharField(max_length=5, verbose_name='性别')
     college = models.CharField(max_length=30, verbose_name='所属学院')
@@ -25,11 +25,14 @@ class Freshman(models.Model):   # 此处继承AbstractUser， 改写auth_user表
     newclass = models.CharField(max_length=20, verbose_name='班级')
     phone = models.CharField(max_length=11, verbose_name='手机')
     qq = models.CharField(max_length=15, verbose_name='QQ号')
-    email = models.CharField(max_length=30, verbose_name='邮箱')
+    email = models.EmailField(max_length=30, verbose_name='邮箱')
     direction = models.CharField(max_length=15, verbose_name='选择方向', choices=(('development', '开发'), ('design', '设计'), ('secretariat', '秘书处')))
+
     # 预约时间， 申请书， 面试时间， 面试地点， 面试得分， 评价， 面试者， 面试结果为后续填入事项，设为不必填
+
     appointment_one = models.DateTimeField(verbose_name='预约时间一', null=True, blank=True)
     appointment_two = models.DateTimeField(verbose_name='预约时间二', null=True, blank=True)
+    appointment_three = models.DateTimeField(verbose_name='预约时间三', null=True, blank=True)
     application = RichTextUploadingField(null=True, blank=True, verbose_name='申请书')
     interview_time = models.DateTimeField(verbose_name='面试时间', null=True, blank=True)
     interview_place = models.CharField(max_length=50, verbose_name='面试地点', blank=True, null=True)
@@ -44,5 +47,3 @@ class Freshman(models.Model):   # 此处继承AbstractUser， 改写auth_user表
 
     def __str__(self):
         return self.newname
-
-
