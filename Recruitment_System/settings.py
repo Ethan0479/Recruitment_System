3801fcf418dff9e44e44b46052b34532ac2a21f9
XINRUDEKEKE <350577273@qq.com>
2019-08-21 11:28:19 +0800

    对接好zxf的报名页面

--- Recruitment_System/settings.py
+++ Recruitment_System/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "recruitment_system",
         "USER": "root",
-        "PASSWORD": "mysql",
+        "PASSWORD": "root",
         "HOST": "127.0.0.1"
     }
 }
--- static/js/lkl_js/sign_up.js
+++ static/js/lkl_js/sign_up.js
@@ -1,7 +1,7 @@
 function appointment() {
-    var appointment_one = document.getElementById("time_1_1").value + document.getElementById("time_2_1").value;
-    var appointment_two = document.getElementById("time_1_2").value + document.getElementById("time_2_2").value;
-    var appointment_three = document.getElementById("time_1_3").value + document.getElementById("time_2_3").value;
+    var appointment_one = document.getElementById("time_1_1").value + "_" + document.getElementById("time_2_1").value;
+    var appointment_two = document.getElementById("time_1_2").value + "_" + document.getElementById("time_2_2").value;
+    var appointment_three = document.getElementById("time_1_3").value + "_" + document.getElementById("time_2_3").value;
     var direction = document.getElementById("button_list").value;
     console.log(direction);
     var csrf_token = getCookie('csrftoken');
