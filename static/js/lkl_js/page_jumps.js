a9c859dd781d7a1164e6918a58b373d993f62191
XINRUDEKEKE <350577273@qq.com>
2019-08-26 22:12:50 +0800

    加入省份，预约时间限制、默认、和回显，加入修改个人信息失败弹框的具体错误信息，个人信息修改的错误提示

--- static/js/lkl_js/page_jumps.js
+++ static/js/lkl_js/page_jumps.js
@@ -131,7 +131,11 @@ function ThirdToForth() {
     document.getElementById('college_list').hidden = true;
     document.getElementById('major_list').hidden = true;
     document.getElementById('apartment_list').hidden = false;
+    document.getElementById('city_list').hidden = false;
     //用于显示变化后的页面的值
+    if (sessionStorage.getItem('city')){
+        document.getElementById('city').value = sessionStorage.getItem('city');
+    }
     if (sessionStorage.getItem('apartment')) {
         document.getElementById('apartment').value = sessionStorage.getItem('apartment');
     }
@@ -139,8 +143,10 @@ function ThirdToForth() {
 }
 function ForthToThird() {
     //保存当前页面数据
+    var city = document.getElementById('city').value;
     var apartment = document.getElementById('apartment').value;
     var dormitory = document.getElementById('dormitory').value;
+    sessionStorage.setItem('city', city);
     sessionStorage.setItem("apartment", apartment);
     sessionStorage.setItem("dormitory", dormitory);
     //显示上/下一页面
@@ -148,6 +154,7 @@ function ForthToThird() {
     document.getElementById('college_list').hidden = false;
     document.getElementById('major_list').hidden = false;
     document.getElementById('apartment_list').hidden = true;
+    document.getElementById('city_list').hidden = true;
     get_major();
     //用于显示变化后的页面的值
     if (sessionStorage.getItem('college')){
@@ -174,8 +181,10 @@ function ForthToThird() {
     document.getElementById('class').value = sessionStorage.getItem('class');
 }
 function Form_Submit() {
+    var city = document.getElementById('city').value;
     var apartment = document.getElementById('apartment').value;
     var dormitory = document.getElementById('dormitory').value;
+    sessionStorage.setItem("city", city);
     sessionStorage.setItem("apartment", apartment);
     sessionStorage.setItem("dormitory", dormitory);
     var csrf_token = getCookie('csrftoken');
@@ -192,6 +201,7 @@ function Form_Submit() {
         'phone' : sessionStorage.getItem('phone'),
         'qq' : sessionStorage.getItem('QQ'),
         'email' : sessionStorage.getItem('email'),
+        'city' : sessionStorage.getItem('city'),
         'apartment' : sessionStorage.getItem('apartment'),
         'dormitory' : sessionStorage.getItem('dormitory'),
         'csrfmiddlewaretoken' : csrf_token},
